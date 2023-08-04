2cb061af6f1c4b9afa74d9b0944b744494af3a3d
JoeL2481 <joel248@hotmail.com.ar>
2023-08-04 15:52:25 -0300

    a

--- Clase 15/index.js
+++ Clase 15/index.js
@@ -2,30 +2,6 @@
 const formButton = document.getElementById('buttonAdd');
 
 
-
-/*formButton.addEventListener('click',function(){
-    const formImput = document.getElementById('textoIngresado');
-    const mainResult = document.getElementById('mainAddResult');
-    const divInside = document.createElement("div");
-    const divSpan = document.createElement("span");
-    const divSpanButton =document.createElement('input');
-    divInside.innerHTML = formImput.value;
-    mainResult.appendChild(divInside);
-    mainResult.appendChild(divSpan);
-    divSpan.appendChild(divSpanButton);
-    divSpanButton.type ='button'
-    divSpanButton.value= 'eliminar';
-    divSpanButton.className ='botonEl';
-
-     const deleteButton = document.querySelector('.botonEl,span')
-
-    deleteButton.addEventListener('click',function(){
-        mainResult.removeChild()
-    })
-
-
-})*/
-
 formButton.addEventListener('click', function() {
     const formInput = document.getElementById('textoIngresado');
     const mainResult = document.getElementById('mainAddResult');
@@ -50,6 +26,7 @@ formButton.addEventListener('click', function() {
     mainResult.appendChild(divInside);
     mainResult.appendChild(divSpan);
   
+    formInput.focus();
    
   });
   
